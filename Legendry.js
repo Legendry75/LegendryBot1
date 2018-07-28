3a565616a55cec07c618559a5382e8d34ef1f0b2
Legendry75 <41837042+Legendry75@users.noreply.github.com>
2018-07-28 20:15:55 +0200

    Update and rename c.js to Legendry.js

--- Legendry.js
+++ Legendry.js
@@ -1,19 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- const prefix = ".";
+
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-    if (message.content === 'Ping') {
-        message.reply('pong');
-      }
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
+client.login('NDcwMjUyNDE5NTM2OTEyMzg0.DjTlUA.-SeV6qsQX5fV0bxUwio_Mjeb_Hs');
 
-
-client.login(NDcwMjUyNDE5NTM2OTEyMzg0.DjTlUA.-SeV6qsQX5fV0bxUwio_Mjeb_Hs);
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     console.log(`in ${client.guilds.size} servers `)
